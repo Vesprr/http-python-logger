435b1ddd397c4b03552464abc478a9663a9d5d92
Muhammad Mujtaba <226957529+mmujtabamain@users.noreply.github.com>
2025-08-24 22:57:40 +0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <fmt/core.h>
+
+int main() {
+    fmt::print("Hello from your C++ template!\n");
+
+    int answer = 42;
+    fmt::print("The answer is: {}\n", answer);
+
+    return 0;
+}
